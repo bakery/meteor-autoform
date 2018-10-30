8ec58702d3b161bb239c13045df2f76848522348
Mark Downey <mark@thebakery.io>
2018-10-30 16:17:01 +0000

    remove bootstrap templates

--- package.js
+++ package.js
@@ -1,19 +1,35 @@
 Package.describe({
-  name: "aldeed:autoform",
-  summary: "Easily create forms with automatic insert and update, and automatic reactive validation.",
-  git: "https://github.com/aldeed/meteor-autoform.git",
-  version: "5.8.1"
+  name: "thebakery:autoform",
+  summary: "Lite version of autoform based on 5.8.1. No bootstrap templates + moment included as NPM dependency.",
+  git: "https://github.com/bakery/meteor-autoform.git",
+  version: "5.8.2"
 });
 
 Package.onUse(function(api) {
   // Dependencies
-  api.versionsFrom(['METEOR@0.9.3', 'METEOR@0.9.4', 'METEOR@1.0']);
+  api.versionsFrom(['METEOR@1.6']);
   // common
-  api.use('aldeed:simple-schema@1.1.0');
+  api.use('aldeed:simple-schema@1.5.4');
   api.use('check');
   // client
-  api.use(['livedata', 'underscore', 'deps', 'templating', 'ui', 'blaze', 'ejson', 'reactive-var', 'reactive-dict', 'random', 'jquery'], 'client');
-  api.use('momentjs:moment@2.10.6', 'client');
+  api.use([
+    'livedata',
+    'underscore',
+    'deps',
+    'templating',
+    'ui',
+    'blaze',
+    'ejson',
+    'reactive-var',
+    'reactive-dict',
+    'random',
+    'jquery'
+], 'client');
+
+  Npm.depends({
+    'moment': '2.21.0',
+  });
+
   api.use('mrt:moment-timezone@0.2.1', 'client', {weak: true});
   api.use('aldeed:moment-timezone@0.4.0', 'client', {weak: true});
   api.use(['aldeed:collection2@2.0.0', 'reload'], 'client', {weak: true});
@@ -144,73 +160,6 @@ Package.onUse(function(api) {
     'components/afQuickFields/afQuickFields.js',
     // event handling
     'autoform-events.js',
-    // bootstrap3 Template - General
-    'templates/bootstrap3/bootstrap3.js',
-    // bootstrap3 Template - Components
-    'templates/bootstrap3/components/quickForm/quickForm.html',
-    'templates/bootstrap3/components/quickForm/quickForm.js',
-    'templates/bootstrap3/components/afArrayField/afArrayField.html',
-    'templates/bootstrap3/components/afArrayField/afArrayField.css',
-    'templates/bootstrap3/components/afFormGroup/afFormGroup.html',
-    'templates/bootstrap3/components/afFormGroup/afFormGroup.js',
-    'templates/bootstrap3/components/afObjectField/afObjectField.html',
-    'templates/bootstrap3/components/afObjectField/afObjectField.js',
-    // bootstrap3 Template - Input Types
-    'templates/bootstrap3/inputTypes/boolean-checkbox/boolean-checkbox.html',
-    'templates/bootstrap3/inputTypes/boolean-radios/boolean-radios.html',
-    'templates/bootstrap3/inputTypes/boolean-radios/boolean-radios.js',
-    'templates/bootstrap3/inputTypes/boolean-select/boolean-select.html',
-    'templates/bootstrap3/inputTypes/button/button.html',
-    'templates/bootstrap3/inputTypes/color/color.html',
-    'templates/bootstrap3/inputTypes/date/date.html',
-    'templates/bootstrap3/inputTypes/datetime/datetime.html',
-    'templates/bootstrap3/inputTypes/datetime-local/datetime-local.html',
-    'templates/bootstrap3/inputTypes/email/email.html',
-    'templates/bootstrap3/inputTypes/month/month.html',
-    'templates/bootstrap3/inputTypes/number/number.html',
-    'templates/bootstrap3/inputTypes/password/password.html',
-    'templates/bootstrap3/inputTypes/radio/radio.html',
-    'templates/bootstrap3/inputTypes/radio/radio.js',
-    'templates/bootstrap3/inputTypes/range/range.html',
-    'templates/bootstrap3/inputTypes/reset/reset.html',
-    'templates/bootstrap3/inputTypes/search/search.html',
-    'templates/bootstrap3/inputTypes/select/select.html',
-    'templates/bootstrap3/inputTypes/select-checkbox/select-checkbox.html',
-    'templates/bootstrap3/inputTypes/select-checkbox/select-checkbox.js',
-    'templates/bootstrap3/inputTypes/select-checkbox-inline/select-checkbox-inline.html',
-    'templates/bootstrap3/inputTypes/select-checkbox-inline/select-checkbox-inline.js',
-    'templates/bootstrap3/inputTypes/select-checkbox-inline/select-checkbox-inline.css',
-    'templates/bootstrap3/inputTypes/select-multiple/select-multiple.html',
-    'templates/bootstrap3/inputTypes/select-radio/select-radio.html',
-    'templates/bootstrap3/inputTypes/select-radio/select-radio.js',
-    'templates/bootstrap3/inputTypes/select-radio-inline/select-radio-inline.html',
-    'templates/bootstrap3/inputTypes/select-radio-inline/select-radio-inline.js',
-    'templates/bootstrap3/inputTypes/select-radio-inline/select-radio-inline.css',
-    'templates/bootstrap3/inputTypes/submit/submit.html',
-    'templates/bootstrap3/inputTypes/tel/tel.html',
-    'templates/bootstrap3/inputTypes/text/text.html',
-    'templates/bootstrap3/inputTypes/textarea/textarea.html',
-    'templates/bootstrap3/inputTypes/time/time.html',
-    'templates/bootstrap3/inputTypes/url/url.html',
-    'templates/bootstrap3/inputTypes/week/week.html',
-    // bootstrap3-horizontal Template - General
-    'templates/bootstrap3-horizontal/bootstrap3-horizontal.css',
-    // bootstrap3-horizontal Template - Components
-    'templates/bootstrap3-horizontal/components/quickForm/quickForm.html',
-    'templates/bootstrap3-horizontal/components/quickForm/quickForm.js',
-    'templates/bootstrap3-horizontal/components/afArrayField/afArrayField.html',
-    'templates/bootstrap3-horizontal/components/afArrayField/afArrayField.js',
-    'templates/bootstrap3-horizontal/components/afFormGroup/afFormGroup.html',
-    'templates/bootstrap3-horizontal/components/afFormGroup/afFormGroup.js',
-    'templates/bootstrap3-horizontal/components/afObjectField/afObjectField.html',
-    'templates/bootstrap3-horizontal/components/afObjectField/afObjectField.js',
-    // bootstrap3-horizontal Template - Input Types
-    'templates/bootstrap3-horizontal/inputTypes/boolean-checkbox/boolean-checkbox.html',
-    'templates/bootstrap3-horizontal/inputTypes/boolean-checkbox/boolean-checkbox.js',
-    // bootstrap3-inline Template
-    'templates/bootstrap3-inline/bootstrap3-inline.html',
-    'templates/bootstrap3-inline/bootstrap3-inline.js',
-    'templates/bootstrap3-inline/bootstrap3-inline.css',
     // plain Template
     'templates/plain/components/quickForm/quickForm.html',
     'templates/plain/components/quickForm/quickForm.js',
